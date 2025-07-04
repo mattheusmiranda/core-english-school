e16c4a86f232976572680e8e817ee3e782f42921
Matheus Miranda <matheusrod59@gmail.com>
2025-07-03 23:15:31 -0300

    feature/lesson-completion-progress - Voltado @SpringBootApplication.

--- bootstrap/src/main/kotlin/core_english_school/CoreEnglishSchoolApplication.kt
+++ bootstrap/src/main/kotlin/core_english_school/CoreEnglishSchoolApplication.kt
@@ -3,7 +3,9 @@ package core_english_school
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@SpringBootApplication
 class CoreEnglishSchoolApplication
+
 
 fun main(args: Array<String>) {
 	runApplication<CoreEnglishSchoolApplication>(*args)
