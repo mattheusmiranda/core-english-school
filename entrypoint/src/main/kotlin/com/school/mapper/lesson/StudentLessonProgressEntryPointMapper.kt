2f3124555ea90ab3cb8d47620bfa4db2cef85274
mattheusmiranda <matheusrod59@gmail.com>
2025-10-09 08:22:56 -0300

    feature/lesson-completion-progress: refactor em de mapper altera porta padrao do mysql remove version do docker-compose.kafka.yml

--- entrypoint/src/main/kotlin/com/school/mapper/lesson/StudentLessonProgressEntryPointMapper.kt
+++ entrypoint/src/main/kotlin/com/school/mapper/lesson/StudentLessonProgressEntryPointMapper.kt
@@ -3,6 +3,8 @@ package com.school.mapper.lesson
 import com.school.domain.StudentLessonProgressDomain
 import com.school.requestModel.lesson.StudentLessonProgressRequest
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 import org.mapstruct.factory.Mappers
 
 @Mapper
@@ -12,5 +14,10 @@ interface StudentLessonProgressEntryPointMapper {
         val INSTANCE: StudentLessonProgressEntryPointMapper = Mappers.getMapper(StudentLessonProgressEntryPointMapper::class.java)
     }
 
+    @Mapping(target = "id", ignore = true)
+    @Mappings(
+        Mapping(source = "studentId", target = "student.id"),
+        Mapping(source = "lessonId", target = "lesson.id")
+    )
     fun toDomainByRequest(classProgressRequest: StudentLessonProgressRequest): StudentLessonProgressDomain
 }
